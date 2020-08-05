4327a98ba8e33d860198515914650e14539e201e
daliborstakic <stakicdalibor64@gmail.com>
2020-08-05 11:38:39 +0200

    Added the initial functions.

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-pass
+# Class Contact
+class Contact(object):
+    def __init__(self, name, surname, number):
+        self.name = name
+        self.surname = surname
+        self.number = number
+
+
+# Function to writeout the contact list
+def write_list(list):
+    pass
+
+# Function to add a new contact
+
+
+def add_contact():
+    pass
+
+# Function to remove a contact
+
+
+def remove_contact():
+    pass
+
+
+# Getting input for a desired action
+run = True
+while run:
+    pass
